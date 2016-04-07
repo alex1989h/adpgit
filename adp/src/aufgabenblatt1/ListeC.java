976bced3ad8977920741d5e9d1b3055780dc9d1a
Alexander Hoffmann <alexander.hoffmann3@haw-hamburg.de>
2016-04-07 20:28:01 +0200

    Aufgabe c vertig

--- adp/src/aufgabenblatt1/ListeC.java
+++ adp/src/aufgabenblatt1/ListeC.java
@@ -2,51 +2,82 @@ package aufgabenblatt1;
 
 public class ListeC<T> implements IListe<T> {
   private int size = 0;
-  private ElementC element;
+  private ElementC element = new ElementC();
 
   @Override
   public void insert(int index, T element) {
-    ElementC hilfElement = this.element;
     if (index >= 0 && index <= size) {
-      if(hilfElement == null){
-        this.element = new ElementC();
-        this.element.einfuegen(element);
-      }else{
-        //while(hilfElement.getNextElement()!=)
-        
+      ElementC hilfElement = this.element;
+      ElementC newElement = new ElementC();
+      newElement.einfuegen(element);
+      for (int i = 0; i < index; i++) {
+        hilfElement = hilfElement.getNextElement();
       }
+      newElement.setNextElement(hilfElement.getNextElement());
+      hilfElement.setNextElement(newElement);
+      size++;
+    } else {
+      throw new IndexOutOfBoundsException();
     }
-
   }
 
   @Override
   public void delete(int index) {
-    // TODO Auto-generated method stub
-
+    if (index >= 0 && index < size) {
+      ElementC hilfElement = this.element;
+      for (int i = 0; i < index; i++) {
+        hilfElement = hilfElement.getNextElement();
+      }
+      hilfElement.setNextElement(hilfElement.getNextElement().getNextElement());
+      size--;
+    } else {
+      throw new IndexOutOfBoundsException();
+    }
   }
 
   @Override
   public int find(T element) {
-    // TODO Auto-generated method stub
-    return 0;
+    if (size > 0) {
+      int index = 0;
+      ElementC hilfElement = this.element.getNextElement();
+      while (hilfElement != null) {
+        if (hilfElement.getElement().equals(element)) {
+          return index;
+        }
+        hilfElement = hilfElement.getNextElement();
+        index++;
+      }
+    }
+    return -1;
   }
 
   @Override
   public Object retrieve(int index) {
-    // TODO Auto-generated method stub
-    return null;
+    if (index >= 0 && index < size) {
+      ElementC hilfElement = this.element;
+      for (int i = 0; i <= index; i++) {
+        hilfElement = hilfElement.getNextElement();
+      }
+      return hilfElement.getElement();
+    } else {
+      throw new IndexOutOfBoundsException();
+    }
   }
 
+  @SuppressWarnings("unchecked")
   @Override
   public void concat(IListe<T> otherListe) {
-    // TODO Auto-generated method stub
-
+    if (otherListe != null) {
+      while (otherListe.size() > 0) {
+        insert(size, (T) otherListe.retrieve(0));
+        otherListe.delete(0);
+      }
+    }
   }
 
   @Override
   public int size() {
-    // TODO Auto-generated method stub
-    return 0;
+    return size;
   }
 
 }
